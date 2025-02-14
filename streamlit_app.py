79d55f29bcd50404fe1107afa6e815b57f77b74a
AbuBakarWorking <abubakarshapatar001@gmail.com>
2025-02-14 22:13:46 +0500

    Update streamlit_app.py
    yes

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ if ingredients_list:
     # st.text(ingredients_list)
 
     ingredients_string = ''
-    for fruit_choosen in ingredients_list:
+    for fruit_chosen in ingredients_list:
         ingredients_string += fruit_choosen + ' '
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
